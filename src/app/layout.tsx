d3a21052d6e2e9b26f248af7bcacde1c6fd6e49c
CDX <80155611+CDX-1@users.noreply.github.com>
2024-11-23 22:49:52 -0500

    Added embed details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Instrument_Sans } from 'next/font/google';
 import Glow from "./components/Glow";
+import Head from "next/head";
 
 const instrumentSans = Instrument_Sans({
   subsets: ['latin']
@@ -18,9 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="description" content="CDX's portfolio showcasing his work with Minecraft server development, Roblox scripting and Discord bot development." />
+        <meta name="author" content="CDX" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="CDX's Portfolio" />
+        <meta property="og:description" content="CDX's portfolio showcasing his work with Minecraft server development, Roblox scripting and Discord bot development." />
+        <meta property="og:url" content="https://cdx.lol" />
+      </Head>
       <body>
         <main className={instrumentSans.className}>
-          <Glow/>
+          <Glow />
           {children}
         </main>
       </body>
