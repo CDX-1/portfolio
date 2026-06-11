caa53ad1584e559e976727c8f4d86baaeb12c773
CDX <80155611+CDX-1@users.noreply.github.com>
2026-06-11 09:13:00 -0400

    added redirects

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,45 @@ const nextConfig: NextConfig = {
         ],
     },
     reactCompiler: true,
+    async redirects() {
+        return [
+            {
+                source: '/resume',
+                destination: '/resume.pdf',
+                permanent: true
+            },
+            {
+                source: '/instagram',
+                destination: 'https://www.instagram.com/awsf__/',
+                permanent: true
+            },
+            {
+                source: '/github',
+                destination: 'https://github.com/CDX-1',
+                permanent: true
+            },
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/awsaf-syed/',
+                permanent: true
+            },
+            {
+                source: '/cosmos',
+                destination: 'https://www.cosmos.so/aw.sf',
+                permanent: true
+            },
+            {
+                source: '/youtube',
+                destination: 'https://www.youtube.com/@rarecdx',
+                permanent: true
+            },
+            {
+                source: '/mail',
+                destination: 'mailto:contact@awsaf.dev',
+                permanent: true
+            }
+        ];
+    }
 };
 
 export default nextConfig;
